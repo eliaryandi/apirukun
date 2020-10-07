f195220be45f0e8cfb46a46c40d53e428967b5c8
eliaryandi <irfaneli10011996$#?>
2020-10-07 10:11:16 +0700

    first

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,111 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\User;
+use Validator;
+use App\Http\Resources\User as UserResource;
+   
+class UserController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        
+        return $this->sendResponse(UserResource::collection($users), 'Users retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+   
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $User = User::create($input);
+   
+        return $this->sendResponse(new UserResource($User), 'User created successfully.');
+    } 
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $User = User::find($id);
+  
+        if (is_null($User)) {
+            return $this->sendError('User not found.');
+        }
+   
+        return $this->sendResponse(new UserResource($User), 'User retrieved successfully.');
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $User)
+    {
+        $input = $request->all();
+
+   
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+   
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $User->name = $input['name'];
+        $User->detail = $input['detail'];
+        $User->save();
+   
+        return $this->sendResponse(new UserResource($User), 'User updated successfully.');
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $User)
+    {
+        $User->delete();
+   
+        return $this->sendResponse([], 'User deleted successfully.');
+    }
+}
